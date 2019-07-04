921f25f0fe08b4c3a4b361c9b482a0cbff5818ba
slagga <957766762@qq.com>
2019-07-04 18:38:07 +0800

    add flask framework

--- script.py
+++ script.py
@@ -1,7 +1,6 @@
 import pyvsystems
 
-# if __name__ == '__main__':
-    
-print('hello world')
-account_info = pyvsystems.Account(chain=pyvsystems.testnet_chain()).get_info()
-print(account_info)
+if __name__ == '__main__':
+    print('hello world')
+    account_info = pyvsystems.Account(chain=pyvsystems.testnet_chain()).get_info()
+    print(account_info)
